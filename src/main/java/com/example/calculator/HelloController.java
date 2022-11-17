1b8a3c7bde88fed8fa3628ad76c0bb548460d541
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-11-17 18:47:24 +0100

    ÃMake calulating function

--- src/main/java/com/example/calculator/HelloController.java
+++ src/main/java/com/example/calculator/HelloController.java
@@ -1,14 +1,56 @@
 package com.example.calculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.text.Text;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    public Text result;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Spinner numberA;
+    @FXML
+    public Spinner numberB;
+
+    public void plusButtonClick(ActionEvent actionEvent) {
+        calculateResult("+",)
+    }
+
+    public void minusButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void timesButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void divideButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void moduloButtonClick(ActionEvent actionEvent) {
+    }
+
+    private double calculateResult(String operator, double operand1, double operand2){
+        double result = 0;
+
+        switch (operator){
+            case "+":
+                result = operand1+operand2;
+                break;
+            case "-":
+                result = operand1- operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "/":
+                result = operand1 / operand2;
+                break;
+            case "%":
+                result = operand1 % operand2;
+                break;
+        }
+
+        return result;
     }
 }
